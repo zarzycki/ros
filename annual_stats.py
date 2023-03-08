845f90b8d51d5740719b17cd51c41e87c46ee679
zarzycki <czarzycki@psu.edu>
2023-03-08 09:50:17 -0500

    add plots to annual stats code

--- annual_stats.py
+++ annual_stats.py
@@ -1,12 +1,17 @@
 import numpy as np
 import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 ## Simple script to check annual snowfall correlations between data products
+add_avg=False
 
 jra = np.genfromtxt('./output/JRA_yearly_total_SWE.csv', delimiter=',')
 l15 = np.genfromtxt('./output/L15_yearly_total_SWE.csv', delimiter=',')
 e3sm = np.genfromtxt('./output/E3SM_yearly_total_SWE.csv', delimiter=',')
 nldas = np.genfromtxt('./output/NLDAS_yearly_total_SWE.csv', delimiter=',')
+
+if (add_avg):
+    mean = np.mean([jra,l15,e3sm,nldas], axis=0)
 
 print(stats.pearsonr(jra, l15))
 print(stats.pearsonr(jra, e3sm))
@@ -14,3 +19,30 @@ print(stats.pearsonr(jra, nldas))
 print(stats.pearsonr(l15, e3sm))
 print(stats.pearsonr(l15, nldas))
 print(stats.pearsonr(e3sm, nldas))
+
+styr=1984
+enyr=2005
+years = range(styr, enyr+1, 1)
+
+fig, ax = plt.subplots(1, 1, figsize = (8, 4))
+ax.set_ylim(0, 410)
+ax.margins(x=0)
+ax.xaxis.grid(True, color='gray', linestyle='--', linewidth=0.5, alpha = 0.8)
+
+ax.plot(years, jra, color = "#CC79A7", label = "JRA", linewidth=2.0, alpha = 0.6)
+ax.plot(years, l15, color = "#009E73", label = "L15", linewidth=2.0, alpha = 1.0)
+ax.plot(years, e3sm, color = "#E69F00", label = "E3SM", linewidth=2.0, alpha = 0.8)
+ax.plot(years, nldas, color = "crimson", label = "NLDAS", linewidth=2.0)
+if (add_avg):
+    ax.plot(years, mean, color = "black", linestyle='--', label = "avg", linewidth=1.0)
+
+ax.set_xticks(np.arange(styr,enyr,2))
+
+ax.set_xlabel("Water year")
+ax.set_ylabel("Positive dSWE sum (mm)")
+
+ax.legend()
+ax.set_title("Interannual snowfall totals")
+fig.savefig("./output/SWE_totals.pdf")
+
+plt.close()
