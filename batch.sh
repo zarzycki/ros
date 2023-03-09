49cf439f34256966dad38e880325329f4864b550
zarzycki <czarzycki@psu.edu>
2023-03-09 17:14:52 -0500

    add imagemagick code that concatenates yearly maps to 2x2 for data

--- batch.sh
+++ batch.sh
@@ -21,6 +21,22 @@ cat Events_*csv > supercat.TMP
 mv supercat.TMP supercat.csv
 cd ..
 
+## Created merged pngs
+cd output
+mkdir -p ./merged/
+DPI=500
+figtypes=("SWE" "events" "scatplot")
+for YYYY in {1984..2005}; do
+  for figtype in "${figtypes[@]}"; do
+  convert +append -density ${DPI} L15_${YYYY}_${figtype}.pdf NLDAS_${YYYY}_${figtype}.pdf tmp1.png
+  convert +append -density ${DPI} JRA_${YYYY}_${figtype}.pdf E3SM_${YYYY}_${figtype}.pdf tmp2.png
+  convert -append tmp1.png tmp2.png tmp3.png
+  mv tmp3.png ./merged/merged_${figtype}_${YYYY}.png
+  rm tmp*.png
+  done
+done
+cd ..
+
 ## check correlations
 python check_correlations.py
 cd corr_stats
