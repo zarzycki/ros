fbbe470f3f644fc829223e6357f097a91412b116
zarzycki <czarzycki@psu.edu>
2024-05-30 12:57:02 -0400

    add parallel options assuming use as gnuparallel

--- batch-stats.sh
+++ batch-stats.sh
@@ -4,7 +4,7 @@
 USGS_gauge="01570500"      # 01570500 for SRB, 14211720 for Willamette
 merge_pngs=true
 perform_analysis=true
-auto_domain_climo="FALSE"   # FALSE to reproduce SRB domain, TRUE otherwise
+auto_domain_climo=true     # false to reproduce SRB domain, true otherwise
 force_purge=true
 
 ######
@@ -12,6 +12,20 @@ force_purge=true
 ## Initialize conda and load env
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate ros-metrics
+
+force_serial=false
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --force_serial)
+      force_serial=true
+      shift # past argument
+      ;;
+    *)    # unknown option
+      shift # past argument
+      ;;
+  esac
+done
 
 if [ "$force_purge" == "true" ]; then
   rm -vrf output/*
@@ -22,23 +36,34 @@ fi
 
 set -e # Turn on error checking
 
-## Make directories (note, the py files should do this for you)
-#echo "Making directories..."
-#mkdir -p ./output
-#mkdir -p ./corr_stats
-
 if [ "$perform_analysis" == "true" ]; then
   echo "Doing analysis..."
-  # Absolute
-  python analysis.py L15 -1 "$USGS_gauge"
-  python analysis.py NLDAS -1 "$USGS_gauge"
-  python analysis.py JRA -1 "$USGS_gauge"
-  python analysis.py E3SM -1 "$USGS_gauge"
-  # Normalized
-  python analysis.py L15 95 "$USGS_gauge"
-  python analysis.py NLDAS 95 "$USGS_gauge"
-  python analysis.py JRA 95 "$USGS_gauge"
-  python analysis.py E3SM 95 "$USGS_gauge"
+  analyses=(
+    "L15 -1 $USGS_gauge"
+    "NLDAS -1 $USGS_gauge"
+    "JRA -1 $USGS_gauge"
+    "E3SM -1 $USGS_gauge"
+    "L15 95 $USGS_gauge"
+    "NLDAS 95 $USGS_gauge"
+    "JRA 95 $USGS_gauge"
+    "E3SM 95 $USGS_gauge"
+  )
+
+  run_analysis() {
+    args=$1
+    python analysis.py $args
+  }
+
+  if [ "$force_serial" == "true" ] || ! command -v parallel > /dev/null; then
+    echo "Running in serial mode."
+    for analysis in "${analyses[@]}"; do
+      run_analysis "$analysis"
+    done
+  else
+    echo "GNU Parallel is installed and not forced to serial. Running in parallel mode."
+    export -f run_analysis
+    parallel run_analysis ::: "${analyses[@]}"
+  fi
 fi
 
 echo "Organizing statistics"
@@ -51,19 +76,38 @@ cd ..
 if [ "$merge_pngs" == "true" ]; then
   echo "Creating merged PNGs"
   cd output
-  mkdir -p ./merged/
-  DPI=500
+
   figtypes=("SWE" "events" "scatplot")
-  for YYYY in {1984..2005}; do
-    echo "Merging $YYYY"
-    for figtype in "${figtypes[@]}"; do
-      convert +append -density ${DPI} L15_${YYYY}_${figtype}.pdf NLDAS_${YYYY}_${figtype}.pdf tmp1.png
-      convert +append -density ${DPI} JRA_${YYYY}_${figtype}.pdf E3SM_${YYYY}_${figtype}.pdf tmp2.png
-      convert -append tmp1.png tmp2.png tmp3.png
-      mv tmp3.png ./merged/merged_${figtype}_${YYYY}.png
-      rm tmp*.png
+
+  mkdir -p ./merged/
+
+  # Function to merge at the YYYY level
+  merge_png() {
+    YYYY=$1
+    figtype=$2
+    DPI=500
+    echo "Merging $YYYY for $figtype"
+    convert +append -density ${DPI} L15_${YYYY}_${figtype}.pdf NLDAS_${YYYY}_${figtype}.pdf tmp1_${YYYY}_${figtype}.png
+    convert +append -density ${DPI} JRA_${YYYY}_${figtype}.pdf E3SM_${YYYY}_${figtype}.pdf tmp2_${YYYY}_${figtype}.png
+    convert -append tmp1_${YYYY}_${figtype}.png tmp2_${YYYY}_${figtype}.png tmp3_${YYYY}_${figtype}.png
+    mv tmp3_${YYYY}_${figtype}.png ./merged/merged_${figtype}_${YYYY}.png
+    rm -v tmp?_${YYYY}_${figtype}.png
+  }
+
+  if [ "$force_serial" == "true" ] || ! command -v parallel > /dev/null; then
+    echo "Running in serial mode."
+    for YYYY in {1984..2005}; do
+      for figtype in "${figtypes[@]}"; do
+        merge_png $YYYY $figtype
+      done
     done
-  done
+  else
+    echo "GNU Parallel is installed and not forced to serial. Running in parallel mode."
+    export -f merge_png
+    export DPI
+    parallel merge_png ::: {1984..2005} ::: "${figtypes[@]}"
+  fi
+
   cd ..
 fi
 
--- raw_process/batch-raw.sh
+++ raw_process/batch-raw.sh
@@ -3,12 +3,6 @@
 RAWDIR="/Users/cmz5202/NetCDF/ros/"
 ROSREPO="/Users/cmz5202/Software/ros/"
 
-#####
-
-## Initialize conda and load env
-eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
-conda activate ros-metrics
-
 STARTYR=1985
 ENDYR=2005
 
@@ -30,9 +24,46 @@ MAXLON=-74.0
 # MINLON=-126.0
 # MAXLON=-120.0
 
+######################################################################################
+
+## Initialize conda and load env
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate ros-metrics
+
 OUTDIR="$ROSREPO/netcdf/"
 
-python JRA_Data_Convert_Multiyear.py "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
-python E3SM_Data_Convert.py "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
-python NLDAS_Data_Convert_Multiyear.py "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
-python L15_Data_Convert_Multiyear.py "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
+force_serial=false
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --force_serial)
+      force_serial=true
+      shift # past argument
+      ;;
+    *)    # unknown option
+      shift # past argument
+      ;;
+  esac
+done
+
+convert_data() {
+  script=$1
+  python $script "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
+}
+
+if [ "$force_serial" == "true" ] || ! command -v parallel > /dev/null; then
+  echo "Running in serial mode."
+  python JRA_Data_Convert_Multiyear.py "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
+  python E3SM_Data_Convert.py "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
+  python NLDAS_Data_Convert_Multiyear.py "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
+  python L15_Data_Convert_Multiyear.py "$RAWDIR" "$OUTDIR" "$MINLAT" "$MAXLAT" "$MINLON" "$MAXLON" "$STARTYR" "$ENDYR"
+else
+  echo "GNU Parallel is installed and not forced to serial. Running in parallel mode."
+  export RAWDIR ROSREPO MINLAT MAXLAT MINLON MAXLON STARTYR ENDYR OUTDIR
+  export -f convert_data
+  parallel convert_data ::: \
+    "JRA_Data_Convert_Multiyear.py" \
+    "E3SM_Data_Convert.py" \
+    "NLDAS_Data_Convert_Multiyear.py" \
+    "L15_Data_Convert_Multiyear.py"
+fi
