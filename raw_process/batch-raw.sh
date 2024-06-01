c971ce6ca99a19de5dda719ddefc90829cbe2936
zarzycki <czarzycki@psu.edu>
2024-06-01 13:27:14 -0400

    merge raw and masking drivers

--- raw_process/batch-raw.sh
+++ raw_process/batch-raw.sh
@@ -2,35 +2,36 @@
 
 RAWDIR="/Users/cmz5202/NetCDF/ros/"
 ROSREPO="/Users/cmz5202/Software/ros/"
+BASINSHAPE="WillametteBasin"       # srb, WillametteBasin, SacRB_USGS1802
 
 STARTYR=1985
 ENDYR=2005
 
-## SRB
-MINLAT=39.0
-MAXLAT=44.0
-MINLON=-80.0
-MAXLON=-74.0
-
-## WillametteBasin
-# MINLAT=42.0
-# MAXLAT=47.0
-# MINLON=-126.0
-# MAXLON=-120.0
-
-## California
-# MINLAT=32.0
-# MAXLAT=43.0
-# MINLON=-128.0
-# MAXLON=-113.0
+if [ "$BASINSHAPE" = "srb" ]; then
+  MINLAT=39.0
+  MAXLAT=44.0
+  MINLON=-80.0
+  MAXLON=-74.0
+elif [ "$BASINSHAPE" = "WillametteBasin" ]; then
+  MINLAT=42.0
+  MAXLAT=47.0
+  MINLON=-126.0
+  MAXLON=-120.0
+elif [ "$BASINSHAPE" = "SacRB_USGS1802" ]; then
+  MINLAT=32.0
+  MAXLAT=43.0
+  MINLON=-128.0
+  MAXLON=-113.0
+else
+  echo "Unknown BASINSHAPE: $BASINSHAPE"
+  exit 1
+fi
 
 ######################################################################################
 
 ## Initialize conda and load env
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate ros-metrics
-
-OUTDIR="$ROSREPO/netcdf/"
 
 force_serial=false
 while [[ $# -gt 0 ]]; do
@@ -45,6 +46,10 @@ while [[ $# -gt 0 ]]; do
       ;;
   esac
 done
+
+OUTDIR="$ROSREPO/netcdf/"
+
+echo "Creating merged netcdf files"
 
 convert_data() {
   script=$1
@@ -67,3 +72,38 @@ else
     "NLDAS_Data_Convert_Multiyear.py" \
     "L15_Data_Convert_Multiyear.py"
 fi
+
+cd $OUTDIR
+echo "Masking files"
+
+run_ncl() {
+  model=$1
+  ncl mask_ncl.ncl 'model="'${model}'"' 'repopath="'${ROSREPO}'"' 'basinshape="'${BASINSHAPE}'"'
+}
+
+if [ "$force_serial" == "true" ] || ! command -v parallel > /dev/null; then
+  echo "Running in serial mode."
+  ncl mask_ncl.ncl 'model="NLDAS"' 'repopath="'${ROSREPO}'"' 'basinshape="'${BASINSHAPE}'"'
+  ncl mask_ncl.ncl 'model="L15"' 'repopath="'${ROSREPO}'"' 'basinshape="'${BASINSHAPE}'"'
+  ncl mask_ncl.ncl 'model="E3SM"' 'repopath="'${ROSREPO}'"' 'basinshape="'${BASINSHAPE}'"'
+  ncl mask_ncl.ncl 'model="JRA"' 'repopath="'${ROSREPO}'"' 'basinshape="'${BASINSHAPE}'"'
+else
+  echo "GNU Parallel is installed and not forced to serial. Running in parallel mode."
+  export ROSREPO BASINSHAPE
+  # Load env_parallel to export environment variables
+  source $(which env_parallel.bash)
+  export -f run_ncl
+  env_parallel run_ncl ::: "NLDAS" "L15" "E3SM" "JRA"
+fi
+
+echo "Deleting unmasked files"
+rm -v *_merged.nc
+
+# Loop over files ending in _masked.nc and rename them
+for file in *_masked.nc; do
+  if [ -f "$file" ]; then
+    new_name="${file%.nc}_${BASINSHAPE}.nc"
+    mv "$file" "$new_name"
+    echo "Renamed $file to $new_name"
+  fi
+done
